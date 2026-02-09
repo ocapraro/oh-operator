ee347b0d354abb45a71782c5e150d9d017d58005
ocapraro <oscar@mcapraro.com>
2026-02-09 07:34:59 -0500

    add initial response

--- agents/Operator.py
+++ agents/Operator.py
@@ -7,6 +7,7 @@ class Operator(Agent):
   def __init__(self, contacts:list[Expert]) -> None:
     self._contacts = contacts
     self._initial_prompt="You are an Operator for a Expert Service, a service that connects people with experts to help with their problems. Whenever you are asked a question you must determine if an expert is required or if a generalist can handle it. Respond in the format {decision:\"generalist\"||EXPERT_NAME}. Here are your experts:\n"+"\n".join([str(c) for c in contacts])
+    self._initial_response="{decision:\"generalist\"}"
 
 
 op = Operator([WeatherMan()])
