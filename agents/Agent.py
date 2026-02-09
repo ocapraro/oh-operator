28252ee7c3f3a0b04b24099b0cbb478462d2021f
ocapraro <oscar@mcapraro.com>
2026-02-09 07:27:52 -0500

    add basic operator

---
+++ agents/Agent.py
@@ -0,0 +1,9 @@
+class Agent:
+  _initial_prompt:str
+  _initial_response:str
+
+  def get_initial_prompt(self):
+    return self._initial_prompt
+
+  def get_initial_response(self):
+    return self._initial_response
---
+++ agents/Operator.py
@@ -0,0 +1,14 @@
+from Agent import Agent
+from experts.Expert import Expert
+from experts.weather_man.WeatherMan import WeatherMan
+class Operator(Agent):
+  _contacts:list[Expert]
+
+  def __init__(self, contacts:list[Expert]) -> None:
+    self._contacts = contacts
+    self._initial_prompt="You are an Operator for a Expert Service, a service that connects people with experts to help with their problems. Whenever you are asked a question you must determine if an expert is required or if a generalist can handle it. Respond in the format {decision:\"generalist\"||EXPERT_NAME}. Here are your experts:\n"+"\n".join([str(c) for c in contacts])
+
+
+op = Operator([WeatherMan()])
+
+print(op.get_initial_prompt())
